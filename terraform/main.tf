1af0158fc9ee8be84e125a74da22a92c439efd65
NghiemCanCode <nghiemgankteam@gmail.com>
2025-09-02 21:47:38 +0700

    update terraform config & update .gitignore #terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,34 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project = "var.project"
+  region = "var.region"
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name  = "${local.data_lake_bucket}_${var.project}"
+  location = "var.region"
+  storage_class = "var.storage_class"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.BQ_DATASET
+  project = var.project
+  location = var.region
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "spotify"
+}
+
+variable "project" {
+  description = "GCP-data-pipeline"
+}
+
+variable "region" {
+  description = "Region for GCP resources"
+  default = "asia-southeast1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "spotify"
+}
